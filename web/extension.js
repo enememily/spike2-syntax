75a887a5572219c9b472886390cfed8f766ca719
enememily <77962860+enememily@users.noreply.github.com>
2026-03-25 11:38:25 +0000

    Add files via upload

---
+++ web/extension.js
@@ -0,0 +1,9 @@
+
+// This file is required so VS Code Web recognizes this as a Web Extension.
+// It does not need to do anything for pure syntax highlighting.
+
+export function activate() {
+    console.log("Spike2 Web Extension activated");
+}
+
+export function deactivate() {}
